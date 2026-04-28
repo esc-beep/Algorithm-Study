7674e0ba19ed723dfa50f576cde79536a0e01481
Eunso Choi <93801149+esc-beep@users.noreply.github.com>
2026-04-29 00:31:49 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 작성해주세요
+SELECT child.ITEM_ID, child.ITEM_NAME, child.RARITY
+FROM ITEM_INFO child
+JOIN ITEM_TREE tree
+ON child.ITEM_ID = tree.ITEM_ID
+JOIN ITEM_INFO parent
+ON tree.PARENT_ITEM_ID = parent.ITEM_ID
+WHERE parent.RARITY = 'RARE'
+ORDER BY ITEM_ID DESC;
