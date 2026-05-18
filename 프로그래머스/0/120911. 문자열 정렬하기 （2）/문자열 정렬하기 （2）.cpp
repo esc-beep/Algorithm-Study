cd27330d93726910c3cd2424ec052b7827916625
Eunso Choi <93801149+esc-beep@users.noreply.github.com>
2026-05-18 17:14:09 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string my_string) {
+    for (char &c : my_string) {
+        c = tolower(c);
+    }
+    sort(my_string.begin(), my_string.end());
+    return my_string;
+}
